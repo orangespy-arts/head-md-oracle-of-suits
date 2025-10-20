2b9ac4354c5cf6c04ad49fc2cdd1d7bc7d51aa31
Zhanlan Wang <98122341+2passionfruit@users.noreply.github.com>
2025-10-20 16:00:47 +0200

    circle

--- code/2025-10-20/second-project/sketch.js
+++ code/2025-10-20/second-project/sketch.js
@@ -1,7 +1,29 @@
 function setup() {
-  createCanvas(400, 400);
+  // create a canvas fill the entire window
+  createCanvas(windowWidth, windowHeight);
+  //set the background color to pink
+
 }
 
 function draw() {
-  background(220);
+
+  background(255, 192, 203);
+  for (let i = 0; i < 150; i++) {
+    noFill();
+    // stroke(0, 102, 153, 255 - i * 25);
+    ellipse(width / 2, height / 2, 10 + i * 10);
+  }
+
+
+  translate(mouseX - width / 2, mouseY - height / 2);
+  for (let i = 0; i < 150; i++) {
+    noFill();
+    // stroke(0, 102, 153, 255 - i * 25);
+    ellipse(width / 2, height / 2, 10 + i * 10);
+  }
+}
+
+//create a function resize the canvas when the window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
